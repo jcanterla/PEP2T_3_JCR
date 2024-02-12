00df16bc0c10069a7d6e730ac6a670c9e37beb09
jcanterla <jcanterlarufino@safareyes.es>
2024-02-12 16:57:56 +0100

    clases derivadas

---
+++ src/AvComGran.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class AvComGran extends Aeronave{
+    public AvComGran(String codigo_del_aparato, LocalDate fecha_de_entrega, String linea_de_montaje, Double horas_de_trabajo_previstas, Double horas_de_trabajo_ya_empleadas, Integer capacidad_pasajeros, Double coste, Double precio_venta) {
+        super(codigo_del_aparato, fecha_de_entrega, linea_de_montaje, horas_de_trabajo_previstas, horas_de_trabajo_ya_empleadas, capacidad_pasajeros, coste, precio_venta);
+    }
+}
